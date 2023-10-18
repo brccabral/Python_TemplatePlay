2be063a32105bdfa60ae6c35ebc78083e03b9e1c
Bruno Cabral <cabral.brc@gmail.com>
2023-10-18 15:52:04 -0700

    fix find locations

--- templateplay/tp.py
+++ templateplay/tp.py
@@ -74,29 +74,75 @@ def screenshot(region):
 
 
 class TemplatePosition:
-    def __init__(self, template_img: TemplateImage, x: int, y: int):
+    def __init__(
+        self, template_img: TemplateImage, x: int, y: int, color: Scalar, thickness=2
+    ):
         self.template_img = template_img
         self.x = x
         self.y = y
+        self.color = color
+        self.thickness = thickness
+
+    @property
+    def start_position(self):
+        return (self.x, self.y)
+
+    @property
+    def end_position(self):
+        return (
+            self.x + self.template_img.width,
+            self.y + self.template_img.height,
+        )
+
+    def __repr__(self):
+        value = self.template_img.img_path
+        value += " " + str(self.x) + " " + str(self.y)
+        value += " " + str(self.color)
+        return value
 
 
-def find_templates(screen, template_images: dict[str, Template], group_threshold = 1, epsilon = 0.2):
+def find_templates(
+    screen: MatLike,
+    template_images: dict[str, Template],
+    group_threshold=1,
+    epsilon=0.2,
+):
     positions: List[TemplatePosition] = []
     for name, template in template_images.items():
         for templ_img in template.images:
             result = cv2.matchTemplate(screen, templ_img.img_cv2, cv2.TM_CCOEFF_NORMED)
-            locations = np.where(result >= np.array(templ_img.threshold))
-            if len(locations):
+            yloc, xloc = np.where(result >= np.array(templ_img.threshold))
+            if len(xloc):
                 # filter duplicates
                 rectangles = []
-                for loc in locations:
-                    rectangles.append([int(loc[0]), int(loc[1]), int(templ_img.img_cv2.shape[0]), int(templ_img.img_cv2.shape[1])])
+                for l, x in enumerate(xloc):
+                    rectangles.append(
+                        [
+                            int(x),
+                            int(yloc[l]),
+                            int(templ_img.width),
+                            int(templ_img.height),
+                        ]
+                    )
                     # force a duplication so cv2.groupRectangles don't remove
                     # locations that have only one rectangle
-                    rectangles.append([int(loc[0]), int(loc[1]), int(templ_img.img_cv2.shape[0]), int(templ_img.img_cv2.shape[1])])
-                rectangles, weights = cv2.groupRectangles(rectangles, group_threshold, epsilon)
+                    rectangles.append(
+                        [
+                            int(x),
+                            int(yloc[l]),
+                            int(templ_img.width),
+                            int(templ_img.height),
+                        ]
+                    )
+                rectangles, weights = cv2.groupRectangles(
+                    rectangles, group_threshold, epsilon
+                )
                 for rect in rectangles:
-                    positions.append(TemplatePosition(templ_img, rect[0], rect[1]))
+                    positions.append(
+                        TemplatePosition(
+                            templ_img, rect[0], rect[1], template.rect_color
+                        )
+                    )
     return positions
 
 
